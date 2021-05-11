c8f60fbb7b70f63c5a043af5babce3c82a7ba1d9
fernandocruz01 <83664174+fernandocruz01@users.noreply.github.com>
2021-05-11 12:44:34 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+#Estudiante: David Fernando Cruz Martinez
+#Correo: david.cruz01@correo.usa.edu.co
+#Carrera: Ingenieria de Sistemas y Telecomunicaciones
+#Profesor: John Jairo Corredor Franco
+#MATERIA: Computacion Paralela y Distribuida
+#UNIVERSIDAD SERGIO ARVOLEDA
+#FECHA: 11/05/2021
