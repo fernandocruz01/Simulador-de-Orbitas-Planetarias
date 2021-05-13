65cfe76e83f315ef1e3637b21ad25276fdd7320f
fernandocruz01 <83664174+fernandocruz01@users.noreply.github.com>
2021-05-13 13:45:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,3 +5,24 @@
 #MATERIA: Computacion Paralela y Distribuida
 #UNIVERSIDAD SERGIO ARVOLEDA
 #FECHA: 11/05/2021
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+ext_modules = [
+    Extension('CySimulador',
+              ['CySimulador.pyx'],
+              libraries=['m'],
+              extra_compile_args=['-ffast-math',
+                                  '-fopenmp', '-march=native'],
+              extra_link_args=['-fopenmp']
+              )]
+
+setup(
+    name='CySimulador',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+)
